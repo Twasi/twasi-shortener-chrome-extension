660025db15d7f08f19bbaa61f5f906e21839ad9d
Merlin Westphal <email@merlinw.de>
2020-09-10 09:58:18 +0200

    + Fixed packaging script

--- scripts/create-package.ts
+++ scripts/create-package.ts
@@ -1,9 +1,21 @@
-import {copyFileSync, existsSync, mkdirSync, rmdirSync} from "fs-extra";
+import {copyFileSync, copySync, existsSync, mkdirSync, rmdirSync} from "fs-extra";
 import {join} from "path";
 import {execSync} from "child_process";
+import {ArgumentParser} from "merlins-argument-parser";
+import {readFileSync, writeFileSync} from "fs";
+
+const argParser = new ArgumentParser(process.argv.slice(2));
 
 const basePath = join(__dirname, '..');
 const path = join(basePath, 'package');
+
+if(!argParser.get('noVersionIncrement').asBool()) {
+    const manifest = JSON.parse(readFileSync(join(basePath, 'manifest.json')).toString());
+    let version: Array<string> = manifest.version.split('.');
+    version[version.length - 1] = (1 + parseInt(version[version.length - 1])).toString();
+    manifest.version = version.join('.');
+    writeFileSync(join(basePath, 'manifest.json'), JSON.stringify(manifest, null, 2));
+}
 
 if (existsSync(path)) {
     rmdirSync(path, {recursive: true});
@@ -12,9 +24,9 @@ if (existsSync(path)) {
 mkdirSync(path);
 
 mkdirSync(join(path, 'ui'));
-copyFileSync(join(basePath, 'ui'), join(path, 'ui'));
+copySync(join(basePath, 'ui'), join(path, 'ui'));
 mkdirSync(join(path, 'icons'));
-copyFileSync(join(basePath, 'icons'), join(path, 'icons'));
+copySync(join(basePath, 'icons'), join(path, 'icons'));
 copyFileSync(join(basePath, 'manifest.json'), join(path, 'manifest.json'));
 
 if (!existsSync(join(basePath, 'dist'))) {
@@ -22,6 +34,6 @@ if (!existsSync(join(basePath, 'dist'))) {
 }
 
 mkdirSync(join(path, 'dist'));
-copyFileSync(join(basePath, 'dist'), join(path, 'dist'));
+copySync(join(basePath, 'dist'), join(path, 'dist'));
 
 console.log('Package created at ' + path);

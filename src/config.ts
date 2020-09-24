29bc012c44b548c5897e29ef240571fb1dfab169
DieserMerlin <email@merlinw.de>
2020-09-24 10:02:09 +0200

    + Version increment and config adjust for new deployment

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export default {
-    PAGE_URL: "http://localhost",
-    API_URL: "http://localhost/gql",
-    JWT_GETTER_URL: "http://localhost/extension-authentication/get-jwt"
+    PAGE_URL: "https://twa.si",
+    API_URL: "https://twa.si/gql",
+    JWT_GETTER_URL: "https://twa.si/extension-authentication/get-jwt"
 }
